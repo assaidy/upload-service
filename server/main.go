9d3bf48f8369da9a41cafc559b75280d351e42b8
Ahmad Assaidy <alsaidyxcode@gmail.com>
2024-11-27 18:30:48 +0200

    applyed validation

--- server/main.go
+++ server/main.go
@@ -6,34 +6,29 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/google/uuid"
 )
 
-var blobDir string
+var (
+	blobDir   string
+	Validator = validator.New()
+)
 
-// TODO: validation
 type ChunkRequest struct {
-	ChunkIndex uint      `json:"chunkIndex"` // index specifies the order of the chunk
-	FileID     uuid.UUID `json:"fileId"`
-	Data       []byte    `json:"data"`
-	TotaChunks uint      `json:"totalChunks"`
+	// ChunkIndex specifies the order of the chunk
+	// might be used if accepting multiple chunks concurrently
+    // and then wirte them to the file with correct order
+	ChunkIndex uint      `json:"chunkIndex" validate:"required"`
+	FileID     uuid.UUID `json:"fileId" validate:"required,uuid	"`
+	Data       []byte    `json:"data" validate:"required"`
+	TotaChunks uint      `json:"totalChunks" validate:"required"`
 }
 
 func main() {
-	args := os.Args[1:]
-	if len(args) != 1 {
-		fmt.Fprintf(os.Stderr, "invalid number of arguments.")
-		printUsage()
-		os.Exit(1)
-	}
-	blobDir = args[0]
-
-	if err := os.MkdirAll(blobDir, os.ModePerm); err != nil {
-		fmt.Fprintf(os.Stderr, "failed to create blob dir. error: %v", err)
-		os.Exit(1)
-	}
+	setBlobDir()
 
 	server := fiber.New(fiber.Config{
 		BodyLimit: 1 * 1024 * 1024, // 1 MB
@@ -51,9 +46,10 @@ func handleUpload(c *fiber.Ctx) error {
 		return fiber.ErrBadRequest
 	}
 
-	// TODO: use a buffer for each fielId for to improve performance
-	// use buffer.Write(req.FileId, req.Data, isLastChunk bool)
-	// is last chunk will make the buffer close the file
+	if err := Validator.Struct(req); err != nil {
+		return fiber.ErrBadRequest
+	}
+
 	path := filepath.Join(blobDir, req.FileID.String())
 	fd, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
 	if err != nil {
@@ -69,6 +65,21 @@ func handleUpload(c *fiber.Ctx) error {
 	log.Printf("recieved chunk | %d/%d | %s", req.ChunkIndex, req.TotaChunks, req.FileID)
 
 	return c.SendStatus(fiber.StatusOK)
+}
+
+func setBlobDir() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "invalid number of arguments.")
+		printUsage()
+		os.Exit(1)
+	}
+	blobDir = args[0]
+
+	if err := os.MkdirAll(blobDir, os.ModePerm); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create blob dir. error: %v", err)
+		os.Exit(1)
+	}
 }
 
 func printUsage() {
